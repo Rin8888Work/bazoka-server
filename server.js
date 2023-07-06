a71f00d37b0218217d0910bdb2293e941f259583
muoingon <phuocnv106@gmail.com>
2023-07-06 08:34:54 +0700

    init

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, I'm Node.js");
+  res.json({ hello: "Hello, I'm Node.js" });
 });
 
 app.listen(port, () => {
