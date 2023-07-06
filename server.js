b69ca2526db563164d9e4fa511fb6b26a4a2e4f9
muoingon <phuocnv106@gmail.com>
2023-07-06 08:36:16 +0700

    init

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.json({ hello: "Hello, I'm Node.js" });
 });
 
+app.get("/bazoka", (req, res) => {
+  res.json({ bazoka: "bazoka" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
